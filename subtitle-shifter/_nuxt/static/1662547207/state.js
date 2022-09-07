f9d6a634327fae872584e127da12bddb9fc045f9
Antons S <ladeganUP@gmail.com>
2022-09-07 17:33:21 +0300

    subtitile shifter updated

--- subtitle-shifter/_nuxt/static/1662547207/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsubtitle-shifter\u002F_nuxt\u002Fstatic\u002F1662547207",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsubtitle-shifter\u002F",assetsPath:"\u002Fsubtitle-shifter\u002F_nuxt\u002F",cdnURL:a}}}}(null));
